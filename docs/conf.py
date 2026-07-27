b1b9263d41fba2862d68bd126596a7800d77fe10
Michael Steinbaugh <mike@steinbaugh.com>
2026-07-27 14:52:41 -0400

    Apply acidgenomics CSS theme to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ html_theme_options = {
     "github_url": "https://github.com/acidgenomics/py-acidplyr",
     "logo": {"text": "acidplyr"},
 }
+html_css_files = ["https://python.acidgenomics.com/css/sphinx.css"]
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable", None),
